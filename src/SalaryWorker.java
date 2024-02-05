071d6dfe55bef834d8537032ed618a01697779f9
McStudent01 <mcmillcy@mail.uc.edu>
2024-02-04 20:54:20 -0600

    added SalaryWorker.java class

---
+++ src/SalaryWorker.java
@@ -0,0 +1,35 @@
+public class SalaryWorker extends Worker
+{
+
+    private double annualSalary;
+
+    public SalaryWorker(String IDNum, String firstName, String lastName, String title, int YOB, double hourlyPayRate, double annualSalary)
+    {
+        super(IDNum, firstName, lastName, title, YOB, hourlyPayRate);
+        this.annualSalary = annualSalary;
+    }
+
+    public double getAnnualSalary()
+    {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary)
+    {
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double calculateWeeklyPay(double hoursWorked)
+    {
+        return annualSalary / 52;
+    }
+
+    @Override
+    public String displayWeeklyPay(double hoursWorked) {
+        double weeklyPay = calculateWeeklyPay(hoursWorked);
+        return String.format("Weekly Pay Information (Salary Worker):\n" +
+                "Weekly Pay: $%.2f\n" +
+                "This is a fraction of the yearly salary.", weeklyPay);
+    }
+}
